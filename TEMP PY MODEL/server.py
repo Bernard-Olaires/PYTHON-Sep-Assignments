e858ffe1fd7a2c44bd4d5672e0c3956f8f78b29d
Bernard Olaires <bolairesjr@gmail.com>
2022-09-18 22:20:39 -0700

    html tables

--- TEMP PY MODEL/server.py
+++ TEMP PY MODEL/server.py
@@ -1,19 +1,14 @@
 # DO NOT FORGET TO : pipenv install flask
 # AND:  pipenv shell (to get enviroment going)
 
-from flask import Flask, render_template # Import Flask to allow us to create our app
+from flask import Flask  # Import Flask to allow us to create our app
 
 app = Flask(__name__)    # Create a new instance of the Flask class called "app"
 
 @app.route('/')          # The "@" decorator associates this route with the function immediately following
-def index():
-    users = [
-    {'first_name' : 'Michael', 'last_name' : 'Choi'},
-    {'first_name' : 'John', 'last_name' : 'Supsupin'},
-    {'first_name' : 'Mark', 'last_name' : 'Guillen'},
-    {'first_name' : 'KB', 'last_name' : 'Tonel'}
-    ]
-    return render_template("index.html", users=users)
+def hello_world():
+    return 'Hello World!'  # Return the string 'Hello World!' as a response
+
 
 
 
