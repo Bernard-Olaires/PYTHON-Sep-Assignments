a509b4d4644c58f6a639fb837759c32bda007eda
Bernard Olaires <bolairesjr@gmail.com>
2022-09-18 21:53:59 -0700

    hello_flask list

--- python/flask/fundamentals/hello_flask/server.py
+++ python/flask/fundamentals/hello_flask/server.py
@@ -16,6 +16,16 @@ def success():
 def hello(banana, num): # you have to bring in banana in your method
     return render_template("hello.html", banana=banana, num=num)
 
+@app.route('/lists')
+def render_lists():
+    # Soon enough, we'll get data from a database, but for now, we're hard coding data
+    student_info = [
+        {'name' : 'Michael', 'age' : 35},
+        {'name' : 'John', 'age' : 30 },
+        {'name' : 'Mark', 'age' : 25},
+        {'name' : 'KB', 'age' : 27}
+    ]
+    return render_template("lists.html", random_numbers = [3,1,5], students = student_info)
 
 
 
