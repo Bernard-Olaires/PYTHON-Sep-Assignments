32c8806c0e982450872ff04e8ef5589f7d2babca
Bernard Olaires <bolairesjr@gmail.com>
2022-10-05 18:13:20 -0700

    login registration validation updates

--- assignments/week_5/login_and_registration/flask_app/controllers/users.py
+++ assignments/week_5/login_and_registration/flask_app/controllers/users.py
@@ -1,49 +1,35 @@
 from flask import render_template,redirect,session,request, flash
 from flask_app import app
 from flask_app.models.user import User
-from flask_bcrypt import Bcrypt
-bcrypt = Bcrypt(app)
-
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    if "user_id" not in session:
+        return render_template("index.html")
+    return redirect("/dashboard")
 
-@app.route('/user/register',methods=['POST'])
+@app.route('/user/register', methods=["POST"])
 def register():
     if not User.validate_register(request.form):
         return redirect('/')
-    data ={ 
-        "first_name": request.form['first_name'],
-        "last_name": request.form['last_name'],
-        "email": request.form['email'],
-        "password": bcrypt.generate_password_hash(request.form['password'])
-    }
-    id = User.add_user(data)
-    session['user_id'] = id
-    return redirect('/dashboard')
+    user_id = User.add_user(request.form)
+    session["user_id"] = user_id
+    return redirect("/dashboard")
 
-@app.route('/user/login',methods=['POST'])
+@app.route('/user/login', methods=["POST"])
 def login():
-    user = User.get_by_email(request.form)
-    if not user:
-        flash("Invalid Email","login")
-        return redirect('/')
-    if not bcrypt.check_password_hash(user.password, request.form['password']):
-        flash("Invalid Password","login")
-        return redirect('/')
-    session['user_id'] = user.id
-    return redirect('/dashboard')
+    logged_in_user = User.validate_login(request.form)
+    if not logged_in_user:
+        return redirect("/")
+    session["user_id"] = logged_in_user.id
+    return redirect("/dashboard")
 
 @app.route('/dashboard')
 def dashboard():
-    if 'user_id' not in session:
-        return redirect('/logout')
-    data ={
-        'id': session['user_id']
-    }
-    print(data, "___*******_____")
-    return render_template("dashboard.html", user=User.get_user_by_id(data))
+    if "user_id" not in session:
+        return redirect("/")
+    logged_in_user = User.get_user_by_id(session["user_id"])
+    return render_template("dashboard.html", logged_in_user = logged_in_user)
 
 @app.route('/logout')
 def logout():
